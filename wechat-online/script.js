21365eacc0c5c984d600287acec42a1b4babf66d
huangsx0925 <18011027@stu.moonshotacademy.cn>
2020-12-06 15:15:43 +0800

    bugs fixed for posting comments commit

--- wechat-online/script.js
+++ wechat-online/script.js
@@ -1,7 +1,7 @@
 document.getElementById("post-button").onclick = function() {
-    post = document.getElementById("input-box").value;
-    let li = document.createElement("li");
-    li.innerHTML = post;
-    document.getElementById("chat-list").appendChild(li);
-    post = document.getElementById("input-box").value = "";
+    let post = document.getElementById("input-box").value; // get comment content from input box
+    let li = document.createElement("li"); // create an empty li element
+    li.innerHTML = post; // set the li element's content to the comment from input box
+    document.getElementById("chat-list").appendChild(li); // add this li element to the comment list
+    document.getElementById("input-box").value = ""; // clear the input box
 }
